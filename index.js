a4b0b3243a6d1c72aabedd73e1db276cd22aae70
zhangyunlong <zhangyunlong@baidu.com>
2013-10-19 11:15:58 +0800

    回滚一下，还是应该标记isClass属性，为了更加规范的编码设计

--- index.js
+++ index.js
@@ -6,29 +6,33 @@
 'use strict';
 
 module.exports = function(content, file, conf){
+    var className;
     if(file.isMod){
-        var className;
         var superClass = conf.superClass || 'PhizView';
+        className = 'phiz_view_' + file.getId().replace(/\W/g, '_');
+        content = [
+            '<?php',
+            'class ' + className + ' extends ' + superClass + ' {',
+            '    protected function loadTemplate() {',
+            '        ob_start();',
+            '        ?>' + content + '<?php',
+            '        return ob_get_clean();',
+            '    }',
+            '}'
+        ].join('\n');
+        file.extras.clazz = className;
+    } else if(file.isClass){
         var reg = /"(?:[^\\"\r\n\f]|\\[\s\S])*"|'(?:[^\\'\n\r\f]|\\[\s\S])*'|(?:\/\/|#)[^\r\n\f]+|\/\*[\s\S]*?(?:\*\/|$)|\sclass\s+([a-zA-Z_][a-zA-Z_0-9]*)[\s{]/g;
         content.replace(reg, function(m, $1){
             if(!className && $1){
                 className = $1;
             }
         });
-        if(!className){
-            className = 'phiz_view_' + file.getId().replace(/\W/g, '_');
-            content = [
-                '<?php',
-                'class ' + className + ' extends ' + superClass + ' {',
-                '    protected function loadTemplate() {',
-                '        ob_start();',
-                '        ?>' + content + '<?php',
-                '        return ob_get_clean();',
-                '    }',
-                '}'
-            ].join('\n');
+        if(className){
+            file.extras.clazz = className;
+        } else {
+            fis.log.error('undefined class name of file [' + file.subpath + ']');
         }
-        file.extras.clazz = className;
     }
     return content;
 };
